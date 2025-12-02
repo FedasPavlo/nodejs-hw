dae9bda6f109a369f8930281534d57f0d6af02b2
Pavlo Fedas <pavlofedas25@gmail.com>
2025-12-02 07:31:26 +0200

    3hw

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,6 +1,6 @@
 import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
-import { TAGS } from '../contacts/tags.js';
+import { TAGS } from '../constants/tags.js';
 
 export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
