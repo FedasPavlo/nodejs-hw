79ab36ecb2912439fe482a7f2c1fbc69f677fdda
Pavlo Fedas <pavlofedas25@gmail.com>
2025-12-05 23:48:23 +0200

    fix bags

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -156,7 +156,7 @@ export const resetPassword = async (req, res) => {
 
   const user = await User.findOne({ _id: payload.sub, email: payload.email});
   if (!user) {
-    throw createHttpError(401, "User not found");
+    throw createHttpError(404, "User not found");
   }
 
    const hashedPassword = await bcrypt.hash(password, 10);
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
-import usersRoutes from "./routes/userRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -30,7 +30,7 @@ app.use(cookieParser());
 // ROUTES (шляхи)
 app.use(authRoutes);
 app.use(notesRoutes);
-app.use(usersRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
